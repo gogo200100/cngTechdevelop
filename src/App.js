876083cc45b8bd8eaa7201bd0087a681cef422fb
hanmanjung <201840233@email.daelim.ac.kr>
2023-06-02 20:44:59 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Link, Route, Switch,Routes } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import Main from './pages/MainPage/Main';
+import Sub1 from './pages/SubPage/Sub-intro';
+import Sub2 from './pages/SubPage/Sub-intro2';
+import Sub2_1 from './pages/SubPage/Sub-intro2_1';
+import Sub2_2 from './pages/SubPage/Sub-intro2_2';
+import Sub3 from './pages/SubPage/Sub-intro-3'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        
+      </div>
+      <Routes>
+        <Route path="/main" element={<Main />} />
+        <Route path="/Sub1" element={<Sub1 />} />
+        <Route path="/Sub2" element={<Sub2 />} />
+        <Route path="/Sub3" element={<Sub3 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
